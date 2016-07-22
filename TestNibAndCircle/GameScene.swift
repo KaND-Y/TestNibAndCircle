771d7d755d449ab30a3e78e5948f4745da467624
K <Kai@Kais-MacBook-Air.local>
2016-07-22 11:03:26 -0700

    initial commit
    yep

--- TestNibAndCircle/GameScene.swift
+++ TestNibAndCircle/GameScene.swift
@@ -9,37 +9,55 @@
 import SpriteKit
 
 class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
-        
-        self.addChild(myLabel)
-    }
-    
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
-        
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
-    }
-   
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
-    }
+	override func didMoveToView(view: SKView) {
+		
+		let theNibsArray = ["blackNibSetOne", "blackNibSetTwo", "blackNibSetThree", "blackNibSetFour", "blackNibSetFive"]
+		let theCirclesArray = ["blackRingSetOne", "blackRingSetTwo", "blackRingSetThree", "blackRingSetFour", "blackRingSetFive"]
+		var touchCounter = 0
+		
+		
+		//blackringsettwo was flipped horizontally
+		for ringCreationNum in 0...4{
+			var r = random() % 40
+			//r = 30
+			var rSpeed = Double(r) / 10.0 + 4.0
+			//rSpeed = 6
+			let rotate = SKAction.rotateByAngle(CGFloat(M_PI) * 2, duration: rSpeed)
+			let rotateForever = SKAction.repeatActionForever(rotate)
+			
+			
+			var aRing = SKSpriteNode(texture: SKTexture(imageNamed:theCirclesArray[ringCreationNum]), color: UIColor.blueColor(), size: CGSize(width: 400, height: 400))
+			addChild(aRing)
+			aRing.position.x = view.frame.width / 2
+			aRing.position.y = view.frame.height / 2
+			aRing.runAction(rotateForever)
+			
+			var aNib = SKSpriteNode(texture: SKTexture(imageNamed:theNibsArray[4 - ringCreationNum]), color: UIColor.blueColor(), size: CGSize(width: 400, height: 400))
+			aRing.addChild(aNib)
+			aNib.anchorPoint = CGPointMake(1.0,1.0)
+
+			
+			aNib.position.x = aRing.frame.width / 2
+			aNib.position.y = aRing.frame.height / 2
+			
+
+		}
+		
+		
+	}
+	
+	
+	
+	
+	override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+		/* Called when a touch begins */
+		(print:"screenTouched!")
+		
+			removeAllActions()
+		
+	}
+	
+	override func update(currentTime: CFTimeInterval) {
+		/* Called before each frame is rendered */
+	}
 }
